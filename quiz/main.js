eb9414929e37c5cbd2568f05865528581b6769bf
DanicaBenjamin <danica.benjamin@gmail.com>
2026-04-28 23:22:34 +0530

    Initial Structure

---
+++ quiz/main.js
@@ -0,0 +1,48 @@
+const click = document.getElementById('click');
+if(click) {
+    click.addEventListener('click', function() {
+        window.location.href = "subject.html";
+    });
+}
+
+const ic = document.getElementById('ICCA');
+if(ic) {
+    ic.addEventListener('click', function() {
+        window.location.href = "ICCA.html";
+    });
+}
+
+const ca = document.getElementById('CA');
+if(ca) {
+    ca.addEventListener('click', function() {
+        window.location.href = "CA.html";
+    });
+}
+
+const ds = document.getElementById('DS');
+if(ds) {
+    ds.addEventListener('click', function() {
+        window.location.href = "DS.html";
+    });
+}
+
+const ad = document.getElementById('ADD');
+if(ad) {
+    ad.addEventListener('click', function() {
+        window.location.href = "ADD.html";
+    });
+}
+
+const qe = document.getElementById('QE');
+if(qe) {
+    qe.addEventListener('click', function() {
+        window.location.href = "CA.html";
+    });
+}
+
+const pe = document.getElementById('PE');
+if(pe) {
+    pe.addEventListener('click', function() {
+        window.location.href = "CA.html";
+    });
+}
